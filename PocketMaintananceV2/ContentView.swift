9eaced307ad6c88d9b3e02593ef85b78d9443f79
KremnevViacheslav <rusted2192@gmail.com>
2022-06-10 22:04:23 +0300

    Initial Commit

---
+++ PocketMaintananceV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocketMaintananceV2
+//
+//  Created by Вячеслав Кремнев on 10.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PocketMaintananceV2/PocketMaintananceV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PocketMaintananceV2App.swift
+//  PocketMaintananceV2
+//
+//  Created by Вячеслав Кремнев on 10.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PocketMaintananceV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
